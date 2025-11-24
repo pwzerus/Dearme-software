497240ccc3403bee41cd2d501b8ce03d5546f896
Eshan Kelkar <eshankelkar@galorithm.com>
2025-11-24 02:11:17 -0600

    Add cucumber tests for delete post
    Added cucumber acceptance tests for
    functionality which allows the creator
    to delete their creates posts.

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -1,3 +1,18 @@
+Given('a post exists with:') do |table|
+  # Converts the table into a hash with first column as keys
+  # and second column elements as corresponding values
+  attrs = table.rows_hash
+
+  creator_email = attrs["creator_email"] ? attrs["creator_email"] :
+                                           TEST_USER_EMAIL
+  creator = User.find_by!(email: creator_email)
+
+  Post.create!(
+          title: attrs["title"] ? attrs["title"] : "Some default title",
+          creator: creator
+          )
+end
+
 Then('I should see a new post created with defaults') do
   expect(page).to have_content(@current_user.email)
 
@@ -10,4 +25,9 @@ Then('I should see a new post created with defaults') do
   expect(page).to have_content(
           "Post #{Time.now.strftime("%Y-%m-%d")}"
           )
+end
+
+Given('I visit the view post page for {string}') do |title|
+  p = Post.find_by!(title: title)
+  visit post_path(p)
 end
